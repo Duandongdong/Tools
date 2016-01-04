08a05a44e85c652462e9deee451732f53936aa78
duanjiadong <duanjiadong@605721ca-db71-4c29-8685-90197b5e930a>
2016-01-04 06:55:29 +0000

    增加公用的工具类TimeUtils，ToastHelper

--- trunk/Android/CommUtil/library/src/main/java/com/baigu/util/StringUtils.java
+++ trunk/Android/CommUtil/library/src/main/java/com/baigu/util/StringUtils.java
@@ -1,7 +1,15 @@
 package com.baigu.util;
 
+import android.text.TextUtils;
+
+import com.baigu.util.log.Logger;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -287,5 +295,228 @@ public class StringUtils {
             }
         }
         return new String(source);
+    }
+    public static String token = null;
+
+    /**
+     * 得到一个不为NULL的字符串
+     *
+     * @param str
+     * @return
+     */
+    public static String getNonNullString(String str) {
+        return TextUtils.isEmpty(str) ? "" : str;
+    }
+
+    /**
+     * 得到一个不为NULL的字符串
+     *
+     * @param n
+     * @return
+     */
+    public static String getNonNullString(Long n) {
+        return n == null ? "" : n.toString();
+    }
+
+    /**
+     * 得到一个不为NULL的字符串
+     *
+     * @param n
+     * @return
+     */
+    public static String getNonNullString(Integer n) {
+        return n == null ? "" : n.toString();
+    }
+
+    /**
+     * 得到一个不为NULL的字符串
+     *
+     * @param f
+     * @return
+     */
+    public static String getNonNullString(Float f) {
+        return f == null ? "" : f.toString();
+    }
+
+    /**
+     * 得到一个不为NULL的字符串
+     *
+     * @param d
+     * @return
+     */
+    public static String getNonNullString(Double d) {
+        return d == null ? "" : d.toString();
+    }
+
+    /**
+     * 数字转换为字母（0,1,2... --> A,B,C....AA,AB,AC...AAA,AAB,AAC...）
+     *
+     * @param num
+     * @return
+     */
+    public static String int2MoreChar(int num) {
+        String str = "";
+        if (num < 0) {
+            return str;
+        }
+        if (num > 25) {
+            for (int j = 0; j < num / 26; j++) {
+                str += "A";
+            }
+            num = num % 26;
+        }
+        char c = (char) (num + 65);
+        str += c;
+        return str;
+    }
+
+    /**
+     * String[] 转换 为 String
+     *
+     * @param array
+     * @return
+     */
+    public static String strArray2Str(String[] array) {
+        StringBuffer sb = new StringBuffer();
+        if (array == null) {
+            return sb.toString();
+        }
+        for (String str : array) {
+            if (TextUtils.isEmpty(str)) {
+                continue;
+            }
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 保留指定的小数点位数
+     *
+     * @param decimal
+     * @param f
+     * @return 返回字符串
+     */
+    public static String formatFloat2Str(int decimal, float f) {
+        String str = "";
+        DecimalFormat format;
+        switch (decimal) {
+            case 1: {
+                format = new DecimalFormat("0.0");// 设置输出数值的格式为XX.X
+                break;
+            }
+            case 2: {
+                format = new DecimalFormat("0.00");// 设置输出数值的格式为XX.XX
+                break;
+            }
+            case 3: {
+                format = new DecimalFormat("0.000");// 设置输出数值的格式为XX.XXX
+                break;
+            }
+            default:
+                format = new DecimalFormat("");// 设置输出数值的格式为XX
+                break;
+        }
+        str = format.format(f);
+        return str;
+    }
+
+    /**
+     * 判断字符是否 是数字或字母
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumOrLetter(String str) {
+        boolean flag = false;
+        if (TextUtils.isEmpty(str)) {
+            return flag;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            flag = (current >= 48 && current <= 57) || (current >= 65 && current <= 90)
+                    || (current >= 97 && current <= 122);
+            if (!flag) {
+                break;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 获取一个字符串最后几个字符
+     *
+     * @param str
+     * @param num
+     * @return
+     */
+    public static String getLastNumStr(String str, int num) {
+        if (str == null) {
+            return "";
+        }
+
+        if (str.length() < num) {
+            return str;
+        }
+        str = str.substring(str.length() - num);
+        return str;
+    }
+
+    /**
+     * 字符串 转换 为 int，转换异常 返回 Integer.MIN_VALUE
+     *
+     * @param str
+     * @return
+     */
+    public static int strToInt(String str) {
+        int n = Integer.MIN_VALUE;
+
+        if (TextUtils.isEmpty(str)) {
+            return n;
+        }
+        if (!TextUtils.isDigitsOnly(str)) {
+            return n;
+        }
+        try {
+            n = Integer.valueOf(str);
+        } catch (Exception e) {
+            Logger.e(e, "strToInt(%)s", str);
+        }
+        return n;
+    }
+
+    /**
+     * 比较时间的方法
+     */
+    public static int compare_date(String DATE1) {
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        try {
+            Date dt1 = df.parse(DATE1);
+            Date dt2 = df.parse("2015-01-01  00:00");
+            if (dt1.getTime() > dt2.getTime()) {
+                System.out.println("dt1 在dt2前");
+                return 1;
+            } else if (dt1.getTime() < dt2.getTime()) {
+                System.out.println("dt1在dt2后");
+                return -1;
+            } else {
+                return 0;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * 取得包含token的map集合
+     *
+     * @return 包含token的map集合
+     */
+    public static final HashMap<String, String> getMapToken() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("token", token);
+        return map;
     }
 }
