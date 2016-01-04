052a4be3a5ca0b68d6f13fc7587654762001970e
duanjiadong <duanjiadong@605721ca-db71-4c29-8685-90197b5e930a>
2016-01-04 03:25:19 +0000

    增加公用的工具类

--- trunk/Android/CommUtil/library/src/main/java/com/baigu/util/CommAppUtils.java
+++ trunk/Android/CommUtil/library/src/main/java/com/baigu/util/CommAppUtils.java
@@ -1,25 +1,30 @@
 package com.baigu.util;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.IBinder;
+import android.text.TextUtils;
 import android.view.inputmethod.InputMethodManager;
+
+import java.util.List;
 
 /**
  * AppUtils
  * <ul>
- * <li>{@link AppUtils#isNamedProcess(Context, String)}</li>
+ * <li>{@link CommAppUtils#isNamedProcess(Context, String)}</li>
  * </ul>
  */
-public class AppUtils {
+public class CommAppUtils {
 
-    private AppUtils() {
+    private CommAppUtils() {
         throw new AssertionError();
     }
 
@@ -105,4 +110,68 @@ public class AppUtils {
         }
         return false;
     }
+
+    /**
+     * 接口请求失败信息
+     *
+     * @param e
+     * @return
+     */
+    public static final String buildErrorMsg(Exception e) {
+        return "请求失败!" + (e == null ? "" : e.getMessage());
+    }
+
+    /**
+     * 判断是否有对应的intent
+     *
+     * @param context
+     * @param intent
+     * @return
+     */
+    public static boolean isIntentCallable(Context context, Intent intent) {
+        List<ResolveInfo> list = context.getPackageManager()
+                .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    /**
+     * 判断是否安装对应程序
+     *
+     * @param context
+     * @param packageName
+     * @return
+     */
+    public static boolean isInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * 拨号
+     *
+     * @param context
+     * @param mobile
+     */
+    public static void call(Context context, String mobile) {
+        if (TextUtils.isEmpty(mobile)) {
+            return;
+        }
+        // 叫出拨号程序
+        // Uri uri = Uri.parse("tel:" + mobile);
+        // Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+        // context.startActivity(intent);
+
+        // 直接打电话出去 需要添加打电话权限：
+        // <uses-permission android:name="android.permission.CALL_PHONE" />
+        Uri uri = Uri.parse("tel:" + mobile);
+        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+        context.startActivity(intent);
+    }
+
+
 }
