691b53d12de651e5847ccae44220d02b3747c00c
duanjiadong <duanjiadong@605721ca-db71-4c29-8685-90197b5e930a>
2016-01-12 07:28:56 +0000

    把车主管家中的自定义view   XListView,XListViewFooter,XListViewHeader提到公共类中

--- CommUIKit/library/src/main/java/com/baigu/ui/widget/XListViewHeader.java
+++ CommUIKit/library/src/main/java/com/baigu/ui/widget/XListViewHeader.java
@@ -1,3 +1,10 @@
+/**
+ * @file XListViewHeader.java
+ * @create Apr 18, 2012 5:22:27 PM
+ * @author Maxwin
+ * @description XListView's header
+ */
+
 package com.baigu.ui.widget;
 
 import android.content.Context;
@@ -16,118 +23,131 @@ import com.baigu.ui.R;
 
 
 public class XListViewHeader extends LinearLayout {
-    private LinearLayout mContainer;
-    private ImageView mArrowImageView;
-    private ProgressBar mProgressBar;
-    private TextView mHintTextView;
-    private int mState = STATE_NORMAL;
+	private LinearLayout mContainer;
+	private ImageView mArrowImageView;
+	private ProgressBar mProgressBar;
+	private TextView mHintTextView;
+	private int mState = STATE_NORMAL;
 
-    private Animation mRotateUpAnim;
-    private Animation mRotateDownAnim;
+	private Animation mRotateUpAnim;
+	private Animation mRotateDownAnim;
 
-    private final int ROTATE_ANIM_DURATION = 180;
+	private final int ROTATE_ANIM_DURATION = 180;
 
-    public final static int STATE_NORMAL = 0;
-    public final static int STATE_READY = 1;
-    public final static int STATE_REFRESHING = 2;
+	public final static int STATE_NORMAL = 0;
+	public final static int STATE_READY = 1;
+	public final static int STATE_REFRESHING = 2;
 
-    private String mHintNormal;
-    private String mHintReady;
-    private String mHintLoading;
+	private String mHintNormal;
+	private String mHintReady;
+	private String mHintLoading;
 
-    public XListViewHeader(Context context) {
-        super(context);
-        initView(context);
-    }
+	public XListViewHeader(Context context) {
+		super(context);
+		initView(context);
+	}
 
-    public XListViewHeader(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initView(context);
-    }
+	public XListViewHeader(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView(context);
+	}
 
-    private void initView(Context context) {
-        // 初始情况，设置下拉刷新view高度为0
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0);
-        mContainer = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.xlistview_header, null);
-        addView(mContainer, lp);
-        setGravity(Gravity.BOTTOM);
+	private void initView(Context context) {
+		// 初始情况，设置下拉刷新view高度为0
+		LayoutParams lp = new LayoutParams(
+				LayoutParams.MATCH_PARENT, 0);
+		mContainer = (LinearLayout) LayoutInflater.from(context).inflate(
+				R.layout.xlistview_header, null);
+		addView(mContainer, lp);
+		setGravity(Gravity.BOTTOM);
 
-        mArrowImageView = (ImageView) findViewById(R.id.xlistview_header_arrow);
-        mHintTextView = (TextView) findViewById(R.id.xlistview_header_hint_textview);
-        mProgressBar = (ProgressBar) findViewById(R.id.xlistview_header_progressbar);
+		mArrowImageView = (ImageView) findViewById(R.id.xlistview_header_arrow);
+		mHintTextView = (TextView) findViewById(R.id.xlistview_header_hint_textview);
+		mProgressBar = (ProgressBar) findViewById(R.id.xlistview_header_progressbar);
 
-        mRotateUpAnim = new RotateAnimation(0.0f, -180.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
-        mRotateUpAnim.setFillAfter(true);
-        mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
-        mRotateDownAnim.setFillAfter(true);
+		mRotateUpAnim = new RotateAnimation(0.0f, -180.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateUpAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateUpAnim.setFillAfter(true);
+		mRotateDownAnim = new RotateAnimation(-180.0f, 0.0f,
+				Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+				0.5f);
+		mRotateDownAnim.setDuration(ROTATE_ANIM_DURATION);
+		mRotateDownAnim.setFillAfter(true);
 
-        mHintNormal = context.getResources().getString(R.string.xlistview_header_hint_normal);
-        mHintReady = context.getResources().getString(R.string.xlistview_header_hint_ready);
-        mHintLoading = context.getResources().getString(R.string.xlistview_header_hint_loading);
-    }
+		mHintNormal = context.getResources().getString(
+				R.string.xlistview_header_hint_normal);
+		mHintReady = context.getResources().getString(
+				R.string.xlistview_header_hint_ready);
+		mHintLoading = context.getResources().getString(
+				R.string.xlistview_loading);
+	}
 
-    public void setState(int state) {
-        if (state == mState) return;
+	public void setState(int state) {
+		if (state == mState)
+			return;
 
-        if (state == STATE_REFRESHING) { // 显示进度
-            mArrowImageView.clearAnimation();
-            mArrowImageView.setVisibility(View.GONE);
-            mProgressBar.setVisibility(View.VISIBLE);
-        } else { // 显示箭头图片
-            mArrowImageView.setVisibility(View.VISIBLE);
-            mProgressBar.setVisibility(View.GONE);
-        }
+		if (state == STATE_REFRESHING) { // 显示进度
+			mArrowImageView.clearAnimation();
+			mArrowImageView.setVisibility(View.GONE);
+			mProgressBar.setVisibility(View.VISIBLE);
+		} else { // 显示箭头图片
+			mArrowImageView.setVisibility(View.VISIBLE);
+			mProgressBar.setVisibility(View.GONE);
+		}
 
-        switch (state) {
-            case STATE_NORMAL:
-                if (mState == STATE_READY) {
-                    mArrowImageView.startAnimation(mRotateDownAnim);
-                }
-                if (mState == STATE_REFRESHING) {
-                    mArrowImageView.clearAnimation();
-                }
-                mHintTextView.setText(mHintNormal);
-                break;
-            case STATE_READY:
-                if (mState != STATE_READY) {
-                    mArrowImageView.clearAnimation();
-                    mArrowImageView.startAnimation(mRotateUpAnim);
-                    mHintTextView.setText(mHintReady);
-                }
-                break;
-            case STATE_REFRESHING:
-                mHintTextView.setText(mHintLoading);
-                break;
-            default:
-        }
+		switch (state) {
+		case STATE_NORMAL:
+			if (mState == STATE_READY) {
+				mArrowImageView.startAnimation(mRotateDownAnim);
+			}
+			if (mState == STATE_REFRESHING) {
+				mArrowImageView.clearAnimation();
+			}
+			mHintTextView.setText(mHintNormal);
+			break;
+		case STATE_READY:
+			if (mState != STATE_READY) {
+				mArrowImageView.clearAnimation();
+				mArrowImageView.startAnimation(mRotateUpAnim);
+				mHintTextView.setText(mHintReady);
+			}
+			break;
+		case STATE_REFRESHING:
+			mHintTextView.setText(mHintLoading);
+			break;
+		default:
+		}
 
-        mState = state;
-    }
+		mState = state;
+	}
 
-    public void setVisiableHeight(int height) {
-        if (height < 0) height = 0;
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContainer.getLayoutParams();
-        lp.height = height;
-        mContainer.setLayoutParams(lp);
-    }
+	public void setVisiableHeight(int height) {
+		if (height < 0)
+			height = 0;
+		LayoutParams lp = (LayoutParams) mContainer
+				.getLayoutParams();
+		lp.height = height;
+		mContainer.setLayoutParams(lp);
+	}
 
-    public int getVisiableHeight() {
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) mContainer.getLayoutParams();
-        return lp.height;
-    }
+	public int getVisiableHeight() {
+		LayoutParams lp = (LayoutParams) mContainer
+				.getLayoutParams();
+		return lp.height;
+	}
 
-    public void setHintNormal(String hintNormal) {
-        this.mHintNormal = hintNormal;
-        mHintTextView.setText(mHintNormal);
-    }
+	public void setHintNormal(String hintNormal) {
+		this.mHintNormal = hintNormal;
+		mHintTextView.setText(mHintNormal);
+	}
 
-    public void setHintReady(String hintReady) {
-        this.mHintReady = hintReady;
-    }
+	public void setHintReady(String hintReady) {
+		this.mHintReady = hintReady;
+	}
 
-    public void setHintLoading(String hintLoading) {
-        this.mHintLoading = hintLoading;
-    }
+	public void setHintLoading(String hintLoading) {
+		this.mHintLoading = hintLoading;
+	}
 }
