7371eb79cee0a807d7acc608b89b96dd783876b0
duanjiadong <duanjiadong@605721ca-db71-4c29-8685-90197b5e930a>
2016-01-20 08:23:54 +0000

    AlertDialog修改

--- CommUIKit/library/src/main/java/com/baigu/ui/widget/AlertDialog.java
+++ CommUIKit/library/src/main/java/com/baigu/ui/widget/AlertDialog.java
@@ -1,6 +1,5 @@
 package com.baigu.ui.widget;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -17,149 +16,133 @@ import com.baigu.ui.R;
 
 /**
  * 通用信息提示框
- * 
+ *
  * @author ddc
  * @date: Aug 17, 2014 12:04:09 AM
  */
 public class AlertDialog extends Dialog {
-	private Activity mActivity;
-	public CharSequence mMessage;
-	public CharSequence mSubMessage;
+    public CharSequence mMessage;
+    public CharSequence mSubMessage;
 
-	public CharSequence mPositiveButtonText;
-	public OnClickListener mPositiveButtonListener;
+    public CharSequence mPositiveButtonText;
+    public OnClickListener mPositiveButtonListener;
 
-	public CharSequence mNegativeButtonText;
-	public OnClickListener mNegativeButtonListener;
+    public CharSequence mNegativeButtonText;
+    public OnClickListener mNegativeButtonListener;
 
-	public AlertDialog(Context context) {
-		this(context, R.style.AlertDialog);
-	}
+    public AlertDialog(Context context) {
+        this(context, R.style.AlertDialog);
+    }
 
-	public AlertDialog(Activity activity) {
-		this(activity, R.style.AlertDialog);
-		mActivity = activity;
-	}
 
-	public AlertDialog(Activity activity, int theme) {
-		this((Context) activity, theme);
-		mActivity = activity;
-	}
+    public AlertDialog(Context context, int theme) {
+        super(context, theme);
+        init();
+    }
 
-	public AlertDialog(Context context, int theme) {
-		super(context, theme);
-		init();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_dialog_alert);
 
-		setContentView(R.layout.layout_dialog_alert);
+        initView();
+    }
 
-		initView();
-	}
+    private void init() {
+        setCancelable(true);
+        setCanceledOnTouchOutside(false);
+    }
 
-	private void init() {
-		setCancelable(true);
-		setCanceledOnTouchOutside(false);
-	}
+    private void initView() {
+        TextView messageView = (TextView) findViewById(R.id.txt_message);
 
-	private void initView() {
-		TextView messageView = (TextView) findViewById(R.id.txt_message);
-		if (mActivity != null) {
-			DisplayMetrics  dm = new DisplayMetrics();  
-			getWindow().getWindowManager().getDefaultDisplay().getMetrics(dm); 
-			messageView.setLayoutParams(new LinearLayout.LayoutParams(
-					(int) (dm.widthPixels * 0.8),
-					LinearLayout.LayoutParams.MATCH_PARENT));
-		}
-		if (!TextUtils.isEmpty(mMessage)) {
-			messageView.setText(mMessage);
-		}
-		if (!TextUtils.isEmpty(mSubMessage)) {
-			TextView subMessageView = (TextView) findViewById(R.id.txt_sub_message);
-			subMessageView.setText(mSubMessage);
-			subMessageView.setVisibility(View.VISIBLE);
-		}
 
-		Button positive = (Button) findViewById(R.id.btn_positive);
-		if (!TextUtils.isEmpty(mPositiveButtonText)) {
-			positive.setText(mPositiveButtonText);
-		}
-		if (mPositiveButtonListener != null) {
-			positive.setVisibility(View.VISIBLE);
-			positive.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					mPositiveButtonListener.onClick(AlertDialog.this,
-							DialogInterface.BUTTON_POSITIVE);
-					dismiss();
-				}
-			});
-		}
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindow().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        messageView.setLayoutParams(new LinearLayout.LayoutParams((int) (dm.widthPixels * 0.8), LinearLayout
+                .LayoutParams.MATCH_PARENT));
 
-		Button negative = (Button) findViewById(R.id.btn_negative);
-		if (!TextUtils.isEmpty(mNegativeButtonText)) {
-			negative.setText(mNegativeButtonText);
-		}
-		if (mNegativeButtonListener != null) {
-			negative.setVisibility(View.VISIBLE);
-			negative.setOnClickListener(new View.OnClickListener() {
+        if (!TextUtils.isEmpty(mMessage)) {
+            messageView.setText(mMessage);
+        }
+        if (!TextUtils.isEmpty(mSubMessage)) {
+            TextView subMessageView = (TextView) findViewById(R.id.txt_sub_message);
+            subMessageView.setText(mSubMessage);
+            subMessageView.setVisibility(View.VISIBLE);
+        }
 
-				@Override
-				public void onClick(View v) {
-					mNegativeButtonListener.onClick(AlertDialog.this,
-							DialogInterface.BUTTON_NEGATIVE);
-					dismiss();
-				}
-			});
-		}
-		View divide = findViewById(R.id.divide_line);
-		divide.setVisibility(positive.getVisibility() == View.VISIBLE
-				&& negative.getVisibility() == View.VISIBLE ? View.VISIBLE
-				: View.GONE);
-	}
+        Button positive = (Button) findViewById(R.id.btn_positive);
+        if (!TextUtils.isEmpty(mPositiveButtonText)) {
+            positive.setText(mPositiveButtonText);
+        }
+        if (mPositiveButtonListener != null) {
+            positive.setVisibility(View.VISIBLE);
+            positive.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mPositiveButtonListener.onClick(AlertDialog.this,
+                            DialogInterface.BUTTON_POSITIVE);
+                    dismiss();
+                }
+            });
+        }
 
-	public AlertDialog setMessage(String message) {
-		this.mMessage = message;
-		return this;
-	}
+        Button negative = (Button) findViewById(R.id.btn_negative);
+        if (!TextUtils.isEmpty(mNegativeButtonText)) {
+            negative.setText(mNegativeButtonText);
+        }
+        if (mNegativeButtonListener != null) {
+            negative.setVisibility(View.VISIBLE);
+            negative.setOnClickListener(new View.OnClickListener() {
 
-	public AlertDialog setSubMessage(String message) {
-		this.mSubMessage = message;
-		return this;
-	}
+                @Override
+                public void onClick(View v) {
+                    mNegativeButtonListener.onClick(AlertDialog.this,
+                            DialogInterface.BUTTON_NEGATIVE);
+                    dismiss();
+                }
+            });
+        }
+        View divide = findViewById(R.id.divide_line);
+        divide.setVisibility(positive.getVisibility() == View.VISIBLE
+                && negative.getVisibility() == View.VISIBLE ? View.VISIBLE
+                : View.GONE);
+    }
 
-	public AlertDialog setPositiveButton(OnClickListener listener) {
-		setPositiveButton("", listener);
-		return this;
-	}
+    public AlertDialog setMessage(String message) {
+        this.mMessage = message;
+        return this;
+    }
 
-	public AlertDialog setPositiveButton(CharSequence text,
-			OnClickListener listener) {
-		this.mPositiveButtonText = text;
-		this.mPositiveButtonListener = listener;
-		return this;
-	}
+    public AlertDialog setSubMessage(String message) {
+        this.mSubMessage = message;
+        return this;
+    }
 
-	public AlertDialog setNegativeButton(OnClickListener listener) {
-		setNegativeButton("", listener);
-		return this;
-	}
+    public AlertDialog setPositiveButton(OnClickListener listener) {
+        setPositiveButton("", listener);
+        return this;
+    }
 
-	public AlertDialog setNegativeButton(CharSequence text,
-			OnClickListener listener) {
-		this.mNegativeButtonText = text;
-		this.mNegativeButtonListener = listener;
-		return this;
-	}
+    public AlertDialog setPositiveButton(CharSequence text, OnClickListener listener) {
+        this.mPositiveButtonText = text;
+        this.mPositiveButtonListener = listener;
+        return this;
+    }
 
-	public static AlertDialog createDialog(Context context) {
-		return new AlertDialog(context, R.style.AlertDialog);
-	}
+    public AlertDialog setNegativeButton(OnClickListener listener) {
+        setNegativeButton("", listener);
+        return this;
+    }
 
-	public static AlertDialog createDialog(Activity activity) {
-		return new AlertDialog(activity, R.style.AlertDialog);
-	}
+    public AlertDialog setNegativeButton(CharSequence text, OnClickListener listener) {
+        this.mNegativeButtonText = text;
+        this.mNegativeButtonListener = listener;
+        return this;
+    }
+
+    public static AlertDialog createDialog(Context context) {
+        return new AlertDialog(context, R.style.AlertDialog);
+    }
 }
