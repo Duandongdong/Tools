bf221627a03f8ebee0f42eb949f4708e2724b3e9
dengdingchun <dengdingchun@605721ca-db71-4c29-8685-90197b5e930a>
2016-05-21 19:08:40 +0000

    订单列表详情

--- trunk/Android/CommonsLibrary/library/src/main/java/com/bestgood/commons/network/http/AbsSpiceActivity.java
+++ trunk/Android/CommonsLibrary/library/src/main/java/com/bestgood/commons/network/http/AbsSpiceActivity.java
@@ -3,7 +3,6 @@ package com.bestgood.commons.network.http;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 
 import com.octo.android.robospice.SpiceManager;
 
