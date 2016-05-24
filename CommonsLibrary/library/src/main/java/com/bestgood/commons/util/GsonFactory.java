0dba089c56934fd16ed709534637b6f664513e24
dengdingchun <dengdingchun@605721ca-db71-4c29-8685-90197b5e930a>
2016-05-24 05:15:14 +0000

    图片上传问题修改

--- CommonsLibrary/library/src/main/java/com/bestgood/commons/util/GsonFactory.java
+++ CommonsLibrary/library/src/main/java/com/bestgood/commons/util/GsonFactory.java
@@ -1,14 +1,35 @@
 package com.bestgood.commons.util;
 
+import android.util.Base64;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 
 /**
  * Created by dengdingchun on 16/3/25.
  */
 public class GsonFactory {
+
+    // Using Android's base64 libraries. This can be replaced with any base64 library.
+    private static class ByteArrayToBase64TypeAdapter implements JsonSerializer<byte[]>, JsonDeserializer<byte[]> {
+        public byte[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return Base64.decode(json.getAsString(), Base64.NO_WRAP);
+        }
+
+        public JsonElement serialize(byte[] src, Type typeOfSrc, JsonSerializationContext context) {
+            return new JsonPrimitive(Base64.encodeToString(src, Base64.NO_WRAP));
+        }
+    }
 
     public static Gson buildNormalGson() {
         return new Gson();
@@ -23,10 +44,15 @@ public class GsonFactory {
                         Modifier.FINAL,
                         Modifier.TRANSIENT,
                         Modifier.VOLATILE)
+                .registerTypeHierarchyAdapter(byte[].class, new ByteArrayToBase64TypeAdapter())
                 .create();
     }
 
     public static Gson buildOnlyIncludeExposeAnnotationGson() {
-        return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .registerTypeHierarchyAdapter(byte[].class,
+                        new ByteArrayToBase64TypeAdapter())
+                .create();
     }
 }
